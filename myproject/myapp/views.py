50e1d3c6b1fbf74643ce16e5a5e5e383d2c51a30
felixfaisal <faisalahmedfarooq46@gmail.com>
2020-02-09 17:19:16 +0530

    Added html rendering funtions

--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import os
+
+def hello(request):
+   return render(request,'hello.html', {})
 
 # Create your views here.
--- myproject/myproject/settings.py
+++ myproject/myproject/settings.py
@@ -11,9 +11,11 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+SETTINGS_PATH = os.path.dirname(os.path.dirname(__file__))
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+NEW_PATH = os.path.join(BASE_DIR, 'myapp/template')
 
 
 # Quick-start development settings - unsuitable for production
@@ -37,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'myapp',
 ]
 
 MIDDLEWARE = [
@@ -54,7 +57,7 @@ ROOT_URLCONF = 'myproject.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [NEW_PATH],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
